1c12c9fe43b2ea23b057e5e85c9117481a1235dc
RockyOu <541272124@qq.com>
2017-11-30 17:54:15 +0800

    1,提交代码，HotelMapper 只保留一个方法。getHotelByPrimaryKey();

--- src/main/java/com/yutian/lucene/controller/HotelController.java
+++ src/main/java/com/yutian/lucene/controller/HotelController.java
@@ -1,35 +1,10 @@
 package com.yutian.lucene.controller;
 
 import java.io.IOException;
-import java.io.InputStream;
-
-import org.apache.ibatis.io.Resources;
-import org.apache.ibatis.session.SqlSession;
-import org.apache.ibatis.session.SqlSessionFactory;
-import org.apache.ibatis.session.SqlSessionFactoryBuilder;
-
-import com.yutian.lucene.entity.Hotel;
-import com.yutian.lucene.mapper.HotelMapper;
 
 public class HotelController {
 
 	public static void main(String[] args) throws IOException {
-		String resource = "mybatis-config.xml";
-		InputStream inputStream = Resources.getResourceAsStream(resource);
-		SqlSessionFactory sqlSessionFactory = new SqlSessionFactoryBuilder().build(inputStream);
-		
-		System.out.println(sqlSessionFactory);
-		
-		SqlSession sqlSession = sqlSessionFactory.openSession();
-		try{
-			HotelMapper hotelMapper = sqlSession.getMapper(HotelMapper.class);
-			Hotel hotel = hotelMapper.selectByPrimaryKey(11232);
-			System.out.println("hotel Message:"+hotel);
-			
-		}finally{
-			sqlSession.close();
-		}
-		
 		
 		
 	}
